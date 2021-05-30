08f32680d7c256035654f23fda1041306b67acd2
Iraproza <kolomiira@gmail.com>
2021-05-30 16:02:07 +0300

    edit html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,51 +18,51 @@ class Item{
     selector: 'purchase-app',
     template: `
     <div class='container'>
-      <div class="page-header">
-          <h1> News </h1>
-      </div>
-      <div class=" row">
-          <div class="form-inline col-12">
-              <div class="form-group ">
-                  <div class="">
-                      <input class="form-control" [(ngModel)]="topic" placeholder = "topic" />
-                  </div>
-              </div>
-              <div class="form-group">
-                  <div class="">
-                      <input class="form-control" [(ngModel)]="description" placeholder="description" />
-                  </div>
-              </div>
-              <div class="form-group">
-                  <div class="">
-                      <input class="form-control" [(ngModel)]="image" placeholder="image" />
-                  </div>
-              </div>
-              <div class="form-group">
-                  <div class="">
-                      <button class="btn btn-dark" (click)="addItem(topic, description, image)">Add</button>
-                  </div>
-              </div>
-          </div>
-          <table class="table table-dark table-striped">
-              <thead>
-                  <tr>
-                      <th>Image</th>
-                      <th>Topic</th>
-                      <th>Description</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr *ngFor="let item of items">
-                      <td>
-                        <img src={{item.image}}>
-                      </td>
-                      <td>{{item.topic}}</td>
-                      <td>{{item.description}}</td>
-                  </tr>
-              </tbody>
-          </table>
-      </div>
+        <div class="page-header">
+            <h1> News </h1>
+        </div>
+        <div class=" row">
+            <div class="form-inline col-12">
+                <div class="form-group ">
+                    <div class="">
+                        <input class="form-control" [(ngModel)]="topic" placeholder = "topic" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="">
+                        <input class="form-control" [(ngModel)]="description" placeholder="description" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="">
+                        <input class="form-control" [(ngModel)]="image" placeholder="image" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="">
+                        <button class="btn btn-dark" (click)="addItem(topic, description, image)">Add</button>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Topic</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr *ngFor="let item of items">
+                        <td>
+                            <img src={{item.image}}>
+                        </td>
+                        <td>{{item.topic}}</td>
+                        <td>{{item.description}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>`
 })
 
